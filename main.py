afb4c58fb6ac1f8d7cdeb2561a796562dcd959dd
chunwangpro <wchun1999@gmail.com>
2024-09-22 15:30:08 -0400

    a simple example

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 from remez import *
 from visualization import *
 
-print("Example:\nFor polynomial: f(x) = 4x^4 + 2x^3 - 5x^2 + 8x - 2.5")
+print("\nExample:\nFor polynomial: f(x) = 4x^4 + 2x^3 - 5x^2 + 8x - 2.5")
 print("We want to find optimal uniform approximation of degree 3 on interval [-1, 1].\n")
 print("It can be caculated theoretically as:")
 print("Pn(x) = 2x^3 - x**2 + 8x - 3\n")
 
 
-print("Now let's calculate it using Remez algorithm:")
+print("\n\nNow let's calculate it using Remez algorithm:\n")
 fx = "4 * x**4 + 2 * x**3 - 5 * x**2 + 8 * x - 2.5"
 fx_der = "16 * x**3 + 6 * x**2 - 10 * x + 8"
 interval = [-1, 1]
 px, xn, history = remez(fx, fx_der, interval, n=3, smart_round=True)
-print(f"Pn(x) =", px)
 print("Visualization:")
 visualization(fx, px, xn, history, interval, n=3)
 
 
-print("\n\nFurther, if we use degree of 4, we will get exactly the same polynomial as above.")
+print("\n\n\nFurther, if we use degree of 4, we will get exactly the same polynomial as above.")
 px, xn, history = remez(fx, fx_der, interval, n=4, smart_round=True)
 print(f"Pn(x) =", px)
 
 
 print("\n\nSame results as we increase degree s.t. it >= original.")
 px, xn, history = remez(fx, fx_der, interval, n=6, smart_round=True)
-print(f"Pn(x) =", px)
+print(f"Pn(x) = {px}\n\n")
